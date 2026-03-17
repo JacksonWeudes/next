05e2a620f1cc3a34bae58a4523482d48ef1ac7a4
JacksonWeudes <weudes2024jackson@gmail.com>
2026-03-17 15:58:22 -0300

    imagens, menu e área de produtos

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
+const header = document.querySelector(".header")
+
 function toggleSideMenu(){
     const menuIcon = document.querySelector(".menu-icon")
     const sideMenu = document.querySelector(".side-menu")
+    const overlay = document.querySelector(".overlay")
 
     menuIcon.addEventListener("click", () => {
         menuIcon.classList.toggle("active")
         sideMenu.classList.toggle("active")
+        overlay.classList.toggle("active")
+        
+        if(window.scrollY == 0){
+            header.classList.toggle("active")
+        }
+    })
+}
+
+function activeHeader()
+{
+    window.addEventListener("scroll", () => {
+        if(window.scrollY > 10){
+            header.classList.add("active")
+        }
+        else{
+            header.classList.remove("active")
+        }
     })
 }
 
 document.addEventListener("DOMContentLoaded", () => {
     toggleSideMenu()
+    activeHeader()
 })
